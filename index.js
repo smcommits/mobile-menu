3368f51c14787041f9632f0ad99b287a5c7ea3d0
smcommits <sm.anas@outlook.com>
2021-05-23 23:55:40 +0530

    Add Linter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const menuBar = document.querySelector('.menu-bars')
+const menuBar = document.querySelector('.menu-bars');
 
 menuBar.addEventListener('click', () => {
   console.log('working');
   document.querySelector('.nav-main').classList.toggle('open');
   document.querySelector('.top-header').classList.toggle('stick');
-})
+});
